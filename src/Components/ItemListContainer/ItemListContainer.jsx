e8d8d12ffe9c228ea92e41089faf3b15fb0c880a
Gabriel Briones <gamiz08@hotmail.com>
2023-02-14 15:58:11 -0300

    rutas con nombres de categorias

--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -3,13 +3,17 @@ import { useParams } from 'react-router-dom'
 import { consultarBDD } from "../../utils/funciones.js"
 
 import { ItemList } from '../ItemList/ItemList.jsx'
+import { CategoriasProductos } from "../CategoriasProductos/CategoriasProductos"
 
 export const ItemListContainer = () => {
     const { idCategoria } = useParams()
     const [productos, setProductos] = useState([])
 
     useEffect(() => {
-        if (idCategoria) { 
+        console.log(idCategoria)
+
+        if (idCategoria) {
+
             consultarBDD('../json/productos.json').then(products => {
                 const prods = products.filter(prod => prod.idCategoria === parseInt(idCategoria))
                 const items = ItemList({ prods })
@@ -29,3 +33,40 @@ export const ItemListContainer = () => {
         </div>
     )
 }
+
+
+// import { useState, useEffect } from 'react'
+// import { useParams } from 'react-router-dom'
+// import { consultarBDD } from "../../utils/funciones.js"
+
+// import { ItemList } from '../ItemList/ItemList.jsx'
+// import { CategoriasProductos } from "../CategoriasProductos/CategoriasProductos"
+
+// export const ItemListContainer = () => {
+//     const { idCategoria } = useParams()
+//     const [productos, setProductos] = useState([])
+
+//     useEffect(() => {
+//         console.log(idCategoria)
+
+//         if (idCategoria) {
+
+//             consultarBDD('../json/productos.json').then(products => {
+//                 const prods = products.filter(prod => prod.idCategoria === parseInt(idCategoria))
+//                 const items = ItemList({ prods })
+//                 setProductos(items)
+//             })
+//         } else {
+//             consultarBDD('./json/productos.json').then(prods => {
+//                 const items = ItemList({ prods })
+//                 setProductos(items)
+//             })
+//         }
+//     }, [idCategoria])
+
+//     return (
+//         <div className='row cardProductos'>
+//             {productos}
+//         </div>
+//     )
+// }
